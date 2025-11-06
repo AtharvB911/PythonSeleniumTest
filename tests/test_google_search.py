0ac7c3b08d1fa53653834e72fe3c2ccb2d057295
AtharvB911 <119008305+AtharvB911@users.noreply.github.com>
2025-11-06 21:13:48 +0530

    test debug

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,11 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_google_search():
-    driver = webdriver.Chrome()  # Make sure chromedriver is in PATH
+    driver = webdriver.Chrome()
     driver.get("https://www.google.com")
+
     search_box = driver.find_element(By.NAME, "q")
     search_box.send_keys("Jenkins Selenium Test")
-    search_box.submit()
+    search_box.send_keys(Keys.RETURN)
+
+    # Wait until the title contains the search term
+    WebDriverWait(driver, 10).until(EC.title_contains("Jenkins"))
+
     assert "Jenkins" in driver.title
+
     driver.quit()
